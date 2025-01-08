5826b9112d734258a6056698920056ccdcb12908
karacayunus <yunusemrekaraca6@gmail.com>
2025-01-08 16:57:58 +0300

    Updated

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -10,10 +10,25 @@ namespace MvcKutuphane.Controllers
     {
         // GET: Kategori
         DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); //tabloları tutacak ve propertieslere erişim sağlayacak
-        public ActionResult Index()
+        public ActionResult Index()  //ActionResult'lar ile methot tanımlıyoruz bunları arka planda view sayfasıyla ilişkilendirerek işlem yapıyoruz
+
         {
             var degerler = db.TBLKATEGORI.ToList();
             return View(degerler);
+        }
+
+        [HttpGet]   //sayfa yüklenince bura çalışsın (ki formu bi normal bi boş toplam 2 kere yüklemesin)
+        public ActionResult KategoriEkle()
+        {
+           
+            return View();
+        }
+        [HttpPost]  //veri ekleyince bura çalışsın
+        public ActionResult KategoriEkle(TBLKATEGORI p)  //ekleme için ActionResult oluşturduk
+        {
+            db.TBLKATEGORI.Add(p);   //eklenece kısım
+            db.SaveChanges();         //databaseye kaydetme
+            return View();
         }
     }
 }
