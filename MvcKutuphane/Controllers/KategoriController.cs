2c73fa2611c61af5b2d4ceab17263e52a836d23a
karacayunus <yunusemrekaraca6@gmail.com>
2025-01-08 15:36:52 +0300

    Updated

--- MvcKutuphane/Controllers/KategoriController.cs
+++ MvcKutuphane/Controllers/KategoriController.cs
@@ -3,15 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcKutuphane.Models.Entity;  //Veri tabanınını tabloları kullanmak içi n
 namespace MvcKutuphane.Controllers
 {
     public class KategoriController : Controller
     {
         // GET: Kategori
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); //tabloları tutacak ve propertieslere erişim sağlayacak
         public ActionResult Index()
         {
-            return View();
+            var degerler = db.TBLKATEGORI.ToList();
+            return View(degerler);
         }
     }
 }
