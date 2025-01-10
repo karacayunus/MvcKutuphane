44c6e28d3066ba8cb27b83e95a610299eb5b55ab
karacayunus <yunusemrekaraca6@gmail.com>
2025-01-10 16:57:13 +0300

    Updated

--- MvcKutuphane/Controllers/YazarController.cs
+++ MvcKutuphane/Controllers/YazarController.cs
@@ -40,5 +40,21 @@ namespace MvcKutuphane.Controllers
             db.SaveChanges();
             return RedirectToAction("Index");
         }
+        public ActionResult YazarGetir(int id) 
+        { 
+            var yzr = db.TBLYAZAR.Find(id);
+            return View("YazarGetir", yzr);
+
+        }
+        public ActionResult YazarGuncelle(TBLYAZAR p) 
+        {
+            var yzr = db.TBLYAZAR.Find(p.ID);
+            yzr.AD = p.AD;
+            yzr.SOYAD = p.SOYAD;
+            yzr.DETAY = p.DETAY;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+
+        }
     }
 }
