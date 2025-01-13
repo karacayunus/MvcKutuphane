c0cc0c6131f6fa769e10c9aa086463060b0efb76
karacayunus <yunusemrekaraca6@gmail.com>
2025-01-13 16:55:53 +0300

    Updated

--- MvcKutuphane/Controllers/KitapController.cs
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -3,13 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using MvcKutuphane.Models.Entity;
 namespace MvcKutuphane.Controllers
 {
     public class KitapController : Controller
     {
         // GET: Kitap
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); 
         public ActionResult Index()
+        {
+            var kitaplar = db.TBLKITAP.ToList();
+            return View(kitaplar);
+        }
+
+        [HttpGet]
+        public ActionResult KitapEkle()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult KitapEkle(TBLKITAP p)
         {
             return View();
         }
