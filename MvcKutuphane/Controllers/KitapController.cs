609199f2459b9092bf693bc1fbf864a6c1a5f829
karacayunus <yunusemrekaraca6@gmail.com>
2025-01-14 11:38:16 +0300

    Updated

--- MvcKutuphane/Controllers/KitapController.cs
+++ MvcKutuphane/Controllers/KitapController.cs
@@ -9,7 +9,7 @@ namespace MvcKutuphane.Controllers
     public class KitapController : Controller
     {
         // GET: Kitap
-        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities(); 
+        DBKUTUPHANEEntities db = new DBKUTUPHANEEntities();
         public ActionResult Index()
         {
             var kitaplar = db.TBLKITAP.ToList();
@@ -19,6 +19,30 @@ namespace MvcKutuphane.Controllers
         [HttpGet]
         public ActionResult KitapEkle()
         {
+            List<SelectListItem> deger1 = (from i in db.TBLKATEGORI.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr1 = deger1;
+
+
+            List<SelectListItem> deger2 = (from i in db.TBLYAZAR.ToList()
+                                           select new SelectListItem
+                                           {
+                                               Text = i.AD +" "+ i.SOYAD,
+                                               Value = i.ID.ToString()
+                                           }).ToList();
+            ViewBag.dgr2 = deger2;
+
+
+
+
+
+
+
+
             return View();
         }
         [HttpPost]
